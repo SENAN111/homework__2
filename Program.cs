53263fbcc1e970b4f8986b56ff1938d66dff902a
SENAN111 <behbudovsenan0@gmail.com>
2022-11-25 00:19:04 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Access_modifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
